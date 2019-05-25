e39b00296d094047bfe4012f99fbcca3d0fc8b05
mashjprime <32034289+mashjprime@users.noreply.github.com>
2019-05-25 15:42:21 +0800

    Basic menu

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,27 +1,46 @@
-/**
- * 
- */
 
 /**
+ * Welcome to HangMan!! This is my first independent Java project committed to
+ * git
+ * 
  * @author Mash J Prime
  *
  */
+import java.util.Scanner;
+
 public class HangMan {
 
-	/**
-	 * 
-	 */
-	public HangMan() {
-		// TODO Auto-generated constructor stub
+	private static final Scanner sc = new Scanner(System.in);
+
+	public void menu() {
+		boolean playing = true;
+		int selection = 0;
+		while (playing) {
+			System.out.println("\nPlease choose from the following");
+			System.out.println("1. Play Game");
+			System.out.println("2. Exit");
+			selection = Integer.parseInt(sc.nextLine());
+			
+			switch (selection) {
+			case 1:
+				System.out.println("Playing Game");
+				break;
+			case 2:
+				System.out.println("Closing application");
+				playing = false;
+				break;
+			}
+		}
+
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public HangMan() {
 		System.out.println("Hello World, welcome to HangMan!");
+		menu();
+	}
 
+	public static void main(String[] args) {
+		HangMan hm = new HangMan();
 	}
 
 }
